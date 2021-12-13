71578f17e3add840095cf4767cb001308290b0e7
Raissa <60992306+profaraissa@users.noreply.github.com>
2021-12-13 06:20:03 -0300

    Add files via upload

---
+++ Variaveis Ambiente.sql
@@ -0,0 +1,7 @@
+
+SHOW GLOBAL STATUS LIKE 'Created_tmp_disk_tables';
+SHOW GLOBAL STATUS LIKE 'Created_tmp_tables';
+
+SHOW GLOBAL VARIABLES LIKE 'tmp_table_size';
+SET GLOBAL tmp_table_size = 208003328;
+
